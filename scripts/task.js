f4b292754c96d3500c54da50015cd40493ff498b
Rishitha1238 <141536317+rishitha2827@users.noreply.github.com>
2025-06-10 06:24:55 +0530

    Update task.js

--- scripts/task.js
+++ scripts/task.js
@@ -1,4 +1,4 @@
-const backendURL = 'https://vercel.com/rishithas-projects-00318a93/vizag-portal-backend';
+const backendURL = 'https://vizag-portal-backend.vercel.app';
 const token = localStorage.getItem("token");
 const user = JSON.parse(localStorage.getItem("user"));
 
